ef637f9c6c3617b2860b31e7679cdc1d99059308
Joshua Maserow <joshua@softwarecreations.co.za>
2023-01-26 22:05:20 +0200

    'Throw error if ROOT_URL environment variable is not provided, to prevent infinite loop. Bumped version.'

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
 module.exports = (opts = {}) => {
   const pluginName = 'postcss-absolute-root-url';
+  const { ROOT_URL } = process.env;
+  if (ROOT_URL===undefined) throw new Error(`${pluginName} requires ROOT_URL environment variable.`);
 
   if (opts===null || typeof opts!=='object' || opts.constructor.name!=='Object') throw new Error(`Expected opts to be an object, but it was type:'${typeof opts}', value:${opts}.`);
 
@@ -7,7 +10,6 @@ module.exports = (opts = {}) => {
 
   const replaceImagesUrl = (decl, postcss) => {
     if (decl.value.indexOf('images/') > -1 && decl.value.indexOf('//')===-1) {
-      const { ROOT_URL } = process.env;
       const oldValue = decl.value;
       decl.value = oldValue.replace(/\.*?images\//, `${ROOT_URL}/images/`);
       if (verbose >= 1) console.log(`${pluginName} replaceImagesUrl '${oldValue}' > '${decl.value}'`);
@@ -18,7 +20,6 @@ module.exports = (opts = {}) => {
 
   const replaceFontsUrl = (decl, postcss) => {
     if (decl.value.indexOf('fonts/') > -1 && decl.value.indexOf('//')===-1) {
-      const { ROOT_URL } = process.env;
       const oldValue = decl.value;
       decl.value = oldValue.replace(/\.*?fonts\//, `${ROOT_URL}/fonts/`);
       if (verbose >= 1) console.log(`${pluginName} replaceFontsUrl '${oldValue}' > '${decl.value}'`);
